4c2a6ff09bd5ed1fb218ad2416590e00af075494
simoch10 <36165780+simoch10@users.noreply.github.com>
2024-08-12 17:32:41 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["695719063191814204", ""], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
